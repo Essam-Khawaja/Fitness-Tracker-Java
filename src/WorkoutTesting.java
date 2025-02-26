456b704e0bedad95859d825320f2db9f65116dca
Essam <syedessam345@gmail.com>
2025-02-26 14:30:24 -0700

    Added tests for validateWorkoutPlan

--- src/WorkoutTesting.java
+++ src/WorkoutTesting.java
@@ -1,2 +1,30 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
 public class WorkoutTesting {
+    @Test public void testValidateWorkoutPlanPush() {
+        boolean expected = true;
+        boolean actual = Workout.validateWorkoutPlan("push");
+        assertEquals(expected, actual);
+    }
+
+    @Test public void testValidateWorkoutPlanUpperAllLower() {
+        boolean expected = true;
+        boolean actual = Workout.validateWorkoutPlan("upper");
+        assertEquals(expected, actual);
+    }
+
+    @Test public void testValidateWorkoutPlanLegAllUpper() {
+        boolean expected = true;
+        boolean actual = Workout.validateWorkoutPlan("LEG");
+        assertEquals(expected, actual);
+    }
+
+    @Test public void testValidateWorkoutPlanOutOfBounds() {
+        boolean expected = false;
+        boolean actual = Workout.validateWorkoutPlan("invalidPlan");
+        assertEquals(expected, actual);
+    }
 }
