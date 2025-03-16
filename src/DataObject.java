19cde41528f03ba831b185919973c20c32d77104
Essam <syedessam345@gmail.com>
2025-03-16 00:33:01 -0600

    Sort of added stuff; not really done anything

--- src/DataObject.java
+++ src/DataObject.java
@@ -1,10 +1,33 @@
+import java.io.*;
+import java.util.ArrayList;
+
 public abstract class DataObject {
-    protected void SaveData(){
+    protected void SaveData(ArrayList<Object> saveData){
         if (this.getClass() == Calories.class) {
             // Some code here
+            File file = new File("CaloriesSave.txt");
+            try {
+                FileWriter fileWriter = new FileWriter(file);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                for (Object o : saveData) {
+                    bufferedWriter.write(o.toString() + '\n');
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
         if (this.getClass() == Workout.class) {
             // Some code here
+            File file = new File("WorkoutsSave.txt");
+            try {
+                FileWriter fileWriter = new FileWriter(file);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                for (Object o : saveData) {
+                    bufferedWriter.write(o.toString() + '\n');
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
