b55f38fd364f4a03a8746c51f3ee8a1067763c07
Essam <syedessam345@gmail.com>
2025-03-23 14:28:51 -0600

    Finished SaveTesting.java

--- src/Testing/SaveTesting.java
+++ src/Testing/SaveTesting.java
@@ -33,6 +33,7 @@ public class SaveTesting {
         testUser.addWorkoutData(workout);
 
         Save.SaveData(testUser);
+        Save.SaveNewUser(testUser);
     }
 
     @Test public void testLoad() {
@@ -57,45 +58,12 @@ public class SaveTesting {
         expectedUser.addCalorieData(calorie1);
         expectedUser.addWorkoutData(workout);
 
-        Save.LoadData(actualUser);
+        Save.LoadData(actualUser, "src/Save/Save.csv");
         boolean expected = true;
         boolean actual = actualUser.equals(expectedUser);
         assertEquals(expected, actual);
     }
 
-    @Test public void testLoadOtherUser() {
-        Set set1 = new Set(10, 25.5F);
-        Set set2 = new Set(10, 25.5F);
-        ArrayList<Set> sets = new ArrayList<>();
-        sets.add(set1);
-        sets.add(set2);
-
-        Exercise exercise1 = new Exercise("Preacher Curl", sets);
-        Exercise exercise2 = new Exercise("Bench Press", sets);
-        ArrayList<Exercise> exercises = new ArrayList<>();
-        exercises.add(exercise1);
-        exercises.add(exercise2);
-
-        Workout workout = new Workout(WorkoutPlan.PUSH, exercises);
-
-        Calories calorie1 = new Calories(MealType.MEAL, MealTime.BREAKFAST, "Toast", 300);
-
-        User actualUser = new User("Name2", "Password2", "Email2");
-        User expectedUser = new User("Name", "Password", "Email");
-        expectedUser.addCalorieData(calorie1);
-        expectedUser.addWorkoutData(workout);
-
-        Save.LoadData(actualUser);
-        ArrayList<Calories> actualCalories = actualUser.getCalorieData();
-        ArrayList<Workout> actualWorkouts = actualUser.getWorkoutData();
-        ArrayList<Calories> expectedCalories = expectedUser.getCalorieData();
-        ArrayList<Workout> expectedWorkouts = expectedUser.getWorkoutData();
-
-        System.out.println(actualCalories.toString());
-        System.out.println(actualWorkouts.toString());
-        System.out.println(expectedCalories.toString());
-        System.out.println(expectedWorkouts.toString());
-    }
 
     @Test public void testSaveUserMultipleData(){
         Set set1 = new Set(10, 25.5F);
@@ -115,7 +83,7 @@ public class SaveTesting {
 
         Calories calorie1 = new Calories(MealType.MEAL, MealTime.BREAKFAST, "Toast", 300);
         Calories calorie2 = new Calories(MealType.MEAL, MealTime.LUNCH, "Biryani", 500);
-        Calories calorie3 = new Calories(MealType.SNACK, null, "Oreos", 200);
+        Calories calorie3 = new Calories(MealType.SNACK, MealTime.NULL, "Oreos", 200);
 
         User actualUser = new User("Name3", "Password3", "Email3");
         actualUser.addCalorieData(calorie1);
@@ -125,6 +93,41 @@ public class SaveTesting {
         actualUser.addCalorieData(calorie3);
 
         Save.SaveData(actualUser);
+        Save.SaveNewUser(actualUser);
+    }
+
+    @Test public void testLoadOtherUser() {
+        Set set1 = new Set(10, 25.5F);
+        Set set2 = new Set(10, 25.5F);
+        ArrayList<Set> sets = new ArrayList<>();
+        sets.add(set1);
+        sets.add(set2);
+
+        Exercise exercise1 = new Exercise("Preacher Curl", sets);
+        Exercise exercise2 = new Exercise("Bench Press", sets);
+        ArrayList<Exercise> exercises = new ArrayList<>();
+        exercises.add(exercise1);
+        exercises.add(exercise2);
+
+        Workout workout = new Workout(WorkoutPlan.PUSH, exercises);
+        Workout workout2 = new Workout(WorkoutPlan.PULL, exercises);
+
+        Calories calorie1 = new Calories(MealType.MEAL, MealTime.BREAKFAST, "Toast", 300);
+        Calories calorie2 = new Calories(MealType.MEAL, MealTime.LUNCH, "Biryani", 500);
+        Calories calorie3 = new Calories(MealType.SNACK, MealTime.NULL, "Oreos", 200);
+
+        User actualUser = new User("Name3", "Password3", "Email3");
+        User expectedUser = new User("Name3", "Password3", "Email3");
+        expectedUser.addCalorieData(calorie1);
+        expectedUser.addWorkoutData(workout);
+        expectedUser.addWorkoutData(workout2);
+        expectedUser.addCalorieData(calorie2);
+        expectedUser.addCalorieData(calorie3);
+
+        Save.LoadData(actualUser, "src/Save/Save.csv");
+        boolean expected = true;
+        boolean actual = actualUser.equals(expectedUser);
+        assertEquals(expected, actual);
     }
 
     @Test public void testLoadUserMultipleData(){
@@ -155,7 +158,7 @@ public class SaveTesting {
         expectedUser.addCalorieData(calorie3);
 
         User actualUser = new User("Name3", "Password3", "Email3");
-        Save.LoadData(actualUser);
+        Save.LoadData(actualUser, "src/Save/Save.csv");
 
         boolean expected = true;
         boolean actual = actualUser.equals(expectedUser);
