b8ad4e2246cdf7b54621b4c3cf7170b0482ad769
Essam <syedessam345@gmail.com>
2025-04-07 18:29:38 -0600

    Made a base format for the project

--- src/main/java/MainFiles/ProjectController.java
+++ src/main/java/MainFiles/ProjectController.java
@@ -1,5 +1,58 @@
 package MainFiles;
 
-public class ProjectController {
+import Data.Calories;
+import Data.User;
+import Enums.MealTime;
+import Enums.MealType;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
 
+import java.util.ArrayList;
+
+public class ProjectController {
+    private static final int MAX_CALORIES = 20000;  // Stores the max calories that can be stored for a single item
+    private static final int MAX_EXERCISE_NUMBER = 10;  // Stores the maximum number of exercises that can be stored for a single workout
+    private static final int MAX_SET_NUMBER = 5;    // Stores the maximum number of sets that can be stored for a single exercise
+    private static User user = new User("", "", "");    // This is the user of the menu
+
+    @FXML
+    private VBox mainView;
+
+    @FXML
+    public void showCalorieView() {
+        user.addCalorieData(new Calories(MealType.MEAL, MealTime.LUNCH, "Biryani", 1200));
+        user.addCalorieData(new Calories(MealType.MEAL, MealTime.LUNCH, "Biryani", 1200));
+        TextArea textArea = new TextArea();
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+        textArea.setPrefWidth(600);
+        textArea.setPrefHeight(600);
+        // Retrieve the calorie data
+        ArrayList<Calories> calorieTrackingData = user.getCalorieData();
+        String outputText = "";
+
+        // Check if its empty
+        if (calorieTrackingData.isEmpty()) {
+            outputText += "No calorie data available for today.";
+        } else {
+            // Go through all the array objects and print them out in correct format
+            outputText += "\n===================================\n";
+            outputText += "   TODAY'S CALORIES TRACKING DATA  \n";
+            outputText += "===================================\n";
+            int totalCalories = 0;  // Stores the total of all the calories stored
+            for (Calories data : calorieTrackingData) {
+                outputText += "\n. " + data.getFoodName();
+                outputText += "\n    Type:" + data.getSnackOrMeal();
+                if (!(data.getSnackOrMeal() == MealType.SNACK)) {
+                    outputText += "\n    Time:" + data.getMealTime();
+                }
+                outputText += "\n    Calories:" + data.getCalories() + " kcal";
+                totalCalories += data.getCalories();
+            }
+            outputText += "\n🔥 Today's Total Calories: " + totalCalories + " kcal 🔥\n";
+        }
+        textArea.setText(outputText);
+        mainView.getChildren().add(textArea);
+    }
 }
