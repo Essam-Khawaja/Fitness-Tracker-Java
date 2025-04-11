6ace8f62b2f32f7cbce24f17a81c114d6b72f88c
Essam <syedessam345@gmail.com>
2025-04-11 16:52:09 -0600

    Merged most of the branches - still some errors

--- src/main/java/MainFiles/ProjectController.java
+++ src/main/java/MainFiles/ProjectController.java
@@ -57,6 +57,7 @@ public class ProjectController {
     @FXML
     private Label loginStatusLabel;
 
+
     @FXML
     public void showCalorieView() {
         TextArea textArea = new TextArea();
@@ -131,38 +132,38 @@ public class ProjectController {
 
     @FXML
     private void addExercise() {
-        String name = workoutName.getText();
-        String kgInput = workoutKilograms.getText();
-        String repsInput = workoutReps.getText();
-
-        // Validate inputs using your existing validation methods
-        if (Workout.validateWeightLifted(kgInput) && Workout.validateReps(repsInput)) {
-            double kilograms = Double.parseDouble(kgInput);
-            int reps = Integer.parseInt(repsInput);
-
-            Exercise exercise = new Exercise(name, kilograms, reps);
-            currentWorkout.getExercises().add(exercise);
-            updateSummary();
-
-            workoutName.clear();
-            workoutKilograms.clear();
-            workoutReps.clear();
-        } else {
-            exerciseSummary.appendText("Invalid input, please enter valid values.\n");
-        }
+//        String name = workoutName.getText();
+//        String kgInput = workoutKilograms.getText();
+//        String repsInput = workoutReps.getText();
+//
+//        // Validate inputs using your existing validation methods
+//        if (Workout.validateWeightLifted(kgInput) && Workout.validateReps(repsInput)) {
+//            double kilograms = Double.parseDouble(kgInput);
+//            int reps = Integer.parseInt(repsInput);
+//
+//            Exercise exercise = new Exercise(name, kilograms, reps);
+//            currentWorkout.getExercises().add(exercise);
+//            updateSummary();
+//
+//            workoutName.clear();
+//            workoutKilograms.clear();
+//            workoutReps.clear();
+//        } else {
+//            exerciseSummary.appendText("Invalid input, please enter valid values.\n");
+//        }
     }
 
     private void updateSummary() {
-        StringBuilder summary = new StringBuilder();
-        for (Exercise e : currentWorkout.getExercises()) {
-            summary.append(e.getExerciseName())
-                    .append(" - ")
-                    .append(e.getWeightLifted())
-                    .append("kg x ")
-                    .append(e.getReps())
-                    .append(" reps\n");
-        }
-        exerciseSummary.setText(summary.toString());
+//        StringBuilder summary = new StringBuilder();
+//        for (Exercise e : currentWorkout.getExercises()) {
+//            summary.append(e.getExerciseName())
+//                    .append(" - ")
+//                    .append(e.getWeightLifted())
+//                    .append("kg x ")
+//                    .append(e.getReps())
+//                    .append(" reps\n");
+//        }
+//        exerciseSummary.setText(summary.toString());
     }
 
     @FXML
@@ -237,6 +238,198 @@ public class ProjectController {
         messageArea.setWrapText(true);
         messageArea.setPrefHeight(50);
         mainView.getChildren().add(messageArea);
+    }
+
+
+    @FXML
+    public void Load() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Load File");
+        File file = fileChooser.showSaveDialog(mainView.getScene().getWindow());
+        if (file != null) {
+            Save.LoadData(user, file.getPath());
+        }
+    }
+
+    @FXML
+    public void Save(ActionEvent event) {
+        Save.SaveData(user);
+    }
+
+    @FXML
+    public void SaveSeparateFile(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save File");
+        File file = fileChooser.showSaveDialog(mainView.getScene().getWindow());
+        if (file != null) {
+            if (!file.getName().endsWith(".txt")) {
+                file = new File(file.getAbsolutePath() + ".txt");
+            }
+            Save.SaveSeparateFile(file, user);
+        }
+    }
+
+    @FXML
+    public void Exit(ActionEvent event) {
+        Platform.exit();
+    }
+
+    @FXML
+    public void logout(ActionEvent event) {
+        user = new User("", "", "");
+        MainMenu.setVisible(false);
+        MainMenu.setDisable(true);
+        LoginPage.setVisible(true);
+        LoginPage.setDisable(false);
+    }
+
+    /**
+     *
+     * @param actionEvent
+     * @author Syed Essam Uddin Khawaja
+     */
+    @FXML
+    public void logIn(ActionEvent actionEvent) {
+        boolean loggedIn = false;
+        String username = usernameInput.getText();
+        String password = passwordInput.getText();
+        String email = emailInput.getText();
+
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+
+        if (username.equals("")) {
+            loginStatusLabel.setText("Username is required.");
+            return;
+        } else if (password.equals("")) {
+            loginStatusLabel.setText("Password is required.");
+            return;
+        } else if (email.equals("")) {
+            loginStatusLabel.setText("Email is required.");
+            return;
+        }
+
+        int validateUser = Save.ValidateUser(user);
+
+        if (validateUser == 1) {
+            loggedIn = true;
+        } else if (validateUser == 0) {
+            passwordInput.requestFocus();
+            loginStatusLabel.setText("Password was wrong. Please try again.");
+        } else {
+            passwordInput.requestFocus();
+            usernameInput.requestFocus();
+            emailInput.requestFocus();
+            loginStatusLabel.setText("User with this email does not exist.");
+        }
+
+        if (loggedIn) {
+            LoginPage.setVisible(false);
+            LoginPage.setDisable(true);
+            loginDataPane.setVisible(true);
+            loginDataPane.setDisable(false);
+        }
+    }
+
+    /**
+     *
+     * @param actionEvent
+     * @author Syed Essam Uddin Khawaja
+     */
+    @FXML
+    public void signUp(ActionEvent actionEvent) {
+        boolean signedUp = false;
+
+        String username = usernameInput.getText();
+        String password = passwordInput.getText();
+        String email = emailInput.getText();
+
+        if (username.equals("")) {
+            loginStatusLabel.setText("Username is required.");
+            return;
+        } else if (password.equals("")) {
+            loginStatusLabel.setText("Password is required.");
+            return;
+        } else if (email.equals("")) {
+            loginStatusLabel.setText("Email is required.");
+            return;
+        }
+
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+        int validateUser = Save.ValidateUser(user);
+        if (validateUser == -1) {
+            Save.SaveNewUser(user);
+            signedUp = true;
+        } else {
+            loginStatusLabel.setText("User with this email already exists.");
+            emailInput.requestFocus();
+        }
+
+        if (signedUp) {
+            LoginPage.setVisible(false);
+            LoginPage.setDisable(true);
+            MainMenu.setVisible(true);
+            MainMenu.setDisable(false);
+            menuBar.setDisable(false);
+        }
+    }
+
+    /**
+     *
+     * @param actionEvent
+     * @author Syed Essam Uddin Khawaja
+     */
+    @FXML
+    public void loadPreviousData(ActionEvent actionEvent) {
+        Save.LoadData(user, "src/main/java/Save/Save.csv");
+        loginDataPane.setVisible(false);
+        loginDataPane.setDisable(true);
+        MainMenu.setVisible(true);
+        MainMenu.setDisable(false);
+        menuBar.setDisable(false);
+    }
+
+    /**
+     * Fix this thing when you add status bar
+     * @param actionEvent
+     */
+    @FXML
+    public void loadNewData(ActionEvent actionEvent) {
+        boolean foundUser = Save.LoadData(user, "src/main/java/Save/Save.csv");
+        if (!foundUser) {}
+    }
+
+    /**
+     *
+     * @param actionEvent
+     * @author Syed Essam Uddin Khawaja
+     */
+    @FXML
+    public void moveToMainMenu(ActionEvent actionEvent) {
+        loginDataPane.setVisible(false);
+        loginDataPane.setDisable(true);
+        MainMenu.setVisible(true);
+        MainMenu.setDisable(false);
+        menuBar.setDisable(false);
+    }
+
+    /**
+     * @author Syed Essam Uddin Khawaja
+     */
+    @FXML
+    public void showWorkoutInputPane() {
+        newWorkoutDataPane.setVisible(true);
+    }
+
+    /**
+     * @author Syed Essam Uddin Khawaja
+     */
+    @FXML
+    public void initialize() {
+        menuBar.setDisable(true);
     }
 
 }
